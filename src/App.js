4cedc7617bcd14ce274cfd26ab5f3da4bbcf0437
katelinchong <kychong3@illinois.edu>
2025-10-02 00:38:02 -0500

    Initial CRA setup for MP2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
